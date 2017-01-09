18546f1c1e1f0eeed1ce4423897469687f0d3581
MelissaMejia <melissa.gms93@gmail.com>
2017-01-09 15:42:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
- echo "hola";
+use google\appengine\api\cloud_storage\CloudStorageTools;
+
  if($_SERVER['REQUEST_METHOD']=='POST'){
  
  $image = $_POST['image'];
  $image_name = $_POST['image_name'];
  $decodificarimagen=base64_decode("$image");
- if(file_put_contents("imagenes/".$image_name.".JPG",$decodificarimagen)){ 	
- echo "Imagen subida satisfactoriamente";
- }else{
- echo "Error al subir la imagen";
- }}
 
+$options = ['gs' => ['acl' => 'public-read']];
+$context = stream_context_create($options);
+$fileName = "gs://${imagenes_ubigiis}/".$image_name.".JPG";
+file_put_contents($fileName, $decodificarimagen, 0, $context);
+
+$publicUrl = CloudStorageTools::getPublicUrl($fileName, false);
+
+echo $publicUrl;
   ?>
